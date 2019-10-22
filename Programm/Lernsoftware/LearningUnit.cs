686d8f2bce40648615077ee292442df7a547b0b3
tobia <tobia@LAPTOP-HL8B50QI>
2019-10-22 21:29:55 +0200

    LU / User

--- Programm/Lernsoftware/LearningUnit.cs
+++ Programm/Lernsoftware/LearningUnit.cs
@@ -8,6 +8,63 @@ namespace Lernsoftware
 {
     class LearningUnit
     {
+        private int averageSuccessCB;
+        private DateTime time;
+        private int learningUnitId;
+        private static int idCounter = 0;
+        CardBox cardBox;
+        private List<Register> registerList;
+        private int round; // Attribut muss in Register oder Cardbox
+        public int AverageSuccessCB
+        {
+            get => averageSuccessCB;
+            set => averageSuccessCB = value; 
+        }
+        public int LearningUnitId
+        {
+            get => learningUnitId;
+            set => learningUnitId = value; 
+        }
+        public int Round
+        {
+            get => round;
+            set => round = value; 
+        }
+        public LearningUnit(CardBox cb)
+        {
+            time = DateTime.Now;
+            learningUnitId = idCounter;
+            idCounter++;
+            cardBox = cb;
+            averageSuccessCB = cardBox.CounterSuccess; //in CB ergänzen
+            registerList = cardBox.Registers;
+        }
+        // Methode für den Aufruf der Register und einzelnen Averages
+        public void showRegisters()
+        {
+            foreach (Register register in registerList)
+            {
+                Console.WriteLine(register.RegisterName + ": " +
+                    (register.Counter/register.CounterSuccess)+"%");
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            LearningUnit learningUnit = (LearningUnit)obj;
+            if (learningUnit.LearningUnitId == this.LearningUnitId)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
 
     }
+    
+
 }
--- Programm/Lernsoftware/User.cs
+++ Programm/Lernsoftware/User.cs
@@ -8,9 +8,10 @@ namespace Lernsoftware
 {
     class User
     {
-        private int userId;
-        private string lastName;
-        private string firstName;
+        private int userId; 
+        private static int idCounter = 0; //DB??
+        private string username;
+        private string password; 
         private List<CardBox> cardBoxList;
 
         public int UserId
@@ -18,21 +19,115 @@ namespace Lernsoftware
             get => userId;
             set => userId = value;
         }
-        public string LastName
+        public string Username
         {
-            get => lastName;
-            set => lastName = value;
+            get => username;
+            set => username = value;
         }
-        public string FirstName
+        public string Password
         {
-            get => firstName;
-            set => firstName = value;
+            get => password;
+            set => password = value;
         }
         internal List<CardBox> CardBoxList
         {
             get => cardBoxList;
             set => cardBoxList = value;
         }
+        public User (string name, string pwd)
+        {
+            username = name;
+            password = pwd;
+            userId = idCounter;
+            idCounter++; 
+            cardBoxList = new List<CardBox>(); 
+        }
+        public void changeCardBox (string alt, string neu)
+        {
+            //Datenbank
+        }
+        public Boolean changePassword (string alt, string neu)
+        {
+            //Datenbank
+            return true; 
+        }
+        public Boolean changeUsername (string alt, string neu)
+        {
+            //Datenbank
+            return true; 
+        }
+        public void createNewCardBox(string name)
+        {
+            CardBox cardbox = new CardBox(name);
+            if (cardBoxList.Contains(cardbox))
+            { //FEhlermeldung breits vorhanden, erstezen?
+                    
+            }
+            else
+            {
+                cardBoxList.Add(cardbox);
+                //Erfolgsmeldung
+            }
+            //Datenbank
+        }
+        public void deleteCardBox(string name)
+        {
+            //Auswahl aus Liste oder wird Name eingegeben?
+            CardBox cardbox = (from c in cardBoxList
+                               where c.CardBoxName == name
+                               select c).FirstOrDefault();
+            
+            if (cardbox == null)
+            {
+                //Fehlermeldung: nicht vorhanden
+            }
+
+            else
+            {
+                cardBoxList.Remove(cardbox);
+                //Erfolgmeldung
+            }
+        }
+        public Boolean deleteUser(string name)
+        {
+            //Datenbank
+            return true; 
+        }
+        public Boolean loginUser(string name, string pwd)
+        {
+            // Datenbank
+            return true; 
+        }
+        public void newUser(string name, string pwd)
+        {
+            User neu = new User(name, pwd);
+            if //User wo vorhanden?
+            { //FEhlermeldung breits vorhanden, erstezen?
+
+            }
+            else
+            {
+                //Erfolgsmeldung, Speichern?? 
+            }
+            //Datenbank
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            User user = (User)obj;
+            if (user.UserId == this.UserId
+                && user.Username == this.Username)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
 
     }
 }
